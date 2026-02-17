bb2aabe818e60a8d3e7596fdde58f85fe518ef74
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2026-02-17 15:50:16 +0100

    chore(build): add project build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+# TODO Build for Zlog
+#!\bin\bash
+
+echo "Setting up Zlog"
+wget https://github.com/HardySimpson/zlog/archive/latest-stable.tar.gz
+tar -zxvf latest-stable.tar.gz
+cd zlog-latest-stable
+make
+sudo make install
+echo "Zlog setup" 
